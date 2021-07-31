0d3af7b63684a611e957bfa7b9d639f4052a470a
nathan <nathanpatrick939@gmail.com>
2021-07-31 17:16:31 -0400

    game logic done, css nearly done, game view nearly done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import Game from "./Game.js";
+import GameView from "./GameView.js";
+
 
 let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
 
-console.log(game.turn)
-console.log(game.board)
+GameView.onTileClick = function (i) {
+    console.log(`Tile Clicked: ${i}`)
+};
+
+GameView.onRestartClick = function () {
+    
+};
