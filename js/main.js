bf3976d99ed4b018a05d40471b15e40a1d08f1d1
nathan <nathanpatrick939@gmail.com>
2021-08-04 21:12:23 -0400

    ready to hand in

--- js/main.js
+++ js/main.js
@@ -16,7 +16,22 @@ const winningMessageTextElement = document.querySelector('[data-winning-message-
 const winningMessageElement = document.getElementById('winningMessage');
 const cellElements = document.querySelectorAll('[data-cell]');
 const restartButton = document.getElementById('restartButton');
+const shareButton = document.getElementById('socialShare')
+const shareBlock = document.getElementById('shareBlock')
+const twiterScript = document.getElementById('twiterScript')
+shareButton.addEventListener('click', shareClick)
 restartButton.addEventListener('click', startGame);
+
+let shareElement = `<div class='share show' id='shareBlock'>
+<div class="textbox">Thanks For Playing!</div>
+<div class="link-box">
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="truen">Tweet</a>
+    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+    <script id="twiterScript" async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0" nonce="L5CLMsws"></script>
+</div>
+</div>`
+
 
 let yellowTurn;
 
@@ -91,6 +106,19 @@ function isDraw() {
         cell.classList.contains(YELLOW_CLASS)
     })
 }
+
+
+function shareClick() {
+    fetch('https://platform.twitter.com/widgets.js').then(response => {
+        console.log(response)
+        winningMessageElement.classList.remove('show')
+        window.location.replace('https://twitter.com/share?ref_src=twsrc%5Etfw')
+    })
+}
+
+
+
+
 
 
 
